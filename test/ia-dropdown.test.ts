8a5f9ef7e79744226cc19c3d9da3233fa8bfae3b
Isa Herico Velasco <7840857+iisa@users.noreply.github.com>
2022-05-19 23:32:10 -0700

    more tests

--- test/ia-dropdown.test.ts
+++ test/ia-dropdown.test.ts
@@ -1,9 +1,17 @@
+/* eslint-disable dot-notation */
 import { html, fixture, expect } from '@open-wc/testing';
+import sinon from 'sinon';
 
 import type { IaDropdown, optionInterface } from '../src/ia-dropdown';
 import '../src/ia-dropdown';
 
 describe('IaDropdown', () => {
+  const sandbox = sinon.createSandbox();
+
+  afterEach(() => {
+    sandbox.restore();
+  });
+
   it('is closed at default', async () => {
     const el = await fixture<IaDropdown>(html`<ia-dropdown></ia-dropdown>`);
     expect(el.open).to.be.false;
@@ -22,39 +30,44 @@ describe('IaDropdown', () => {
     expect(caret).to.exist;
   });
 
-  it('caret, when displayed, changes to point up when dropdown is opened', async () => {
-    const el = await fixture<IaDropdown>(
-      html`<ia-dropdown displayCaret></ia-dropdown>`
-    );
+  describe('Toggling', () => {
+    it('has `toggleOptions` function to toggle open dropdown options', async () => {
+      const el = await fixture<IaDropdown>(
+        html`<ia-dropdown displayCaret></ia-dropdown>`
+      );
 
-    expect(el.displayCaret).to.be.true;
-    const caret = el.shadowRoot?.querySelector('span.caret');
-    expect(caret?.querySelector('.caret-down-svg')).to.exist;
-    expect(caret?.querySelector('.caret-up-svg')).to.not.exist;
+      expect(el.open).to.be.false;
+      el.toggleOptions();
+      await el.updateComplete;
 
-    const trigger = el.shadowRoot?.querySelector('button.click-main');
-    expect(trigger).to.exist;
+      expect(el.open).to.be.true;
+    });
+    it('caret, when displayed, changes to point up when dropdown is opened', async () => {
+      const el = await fixture<IaDropdown>(
+        html`<ia-dropdown displayCaret></ia-dropdown>`
+      );
 
-    (trigger as any)?.click(); // TS barks but `click()` is alegit test fn
+      expect(el.displayCaret).to.be.true;
+      const caret = el.shadowRoot?.querySelector('span.caret');
+      expect(caret?.querySelector('.caret-down-svg')).to.exist;
+      expect(caret?.querySelector('.caret-up-svg')).to.not.exist;
 
-    await el.updateComplete;
+      const trigger = el.shadowRoot?.querySelector('button.click-main');
+      expect(trigger).to.exist;
 
-    expect(caret?.querySelector('.caret-down-svg')).to.not.exist;
-    expect(caret?.querySelector('.caret-up-svg')).to.exist;
-    expect(el.open).to.be.true;
+      (trigger as any)?.click(); // TS barks but `click()` is alegit test fn
+
+      await el.updateComplete;
+
+      expect(caret?.querySelector('.caret-down-svg')).to.not.exist;
+      expect(caret?.querySelector('.caret-up-svg')).to.exist;
+      expect(el.open).to.be.true;
+    });
   });
 
   describe('Options', () => {
     it('receives a list of options', async () => {
-      let optionCallbackReceived = false;
       const options: optionInterface[] = [
-        {
-          selectedHandler: () => {
-            optionCallbackReceived = true;
-          },
-          label: 'beep',
-          id: 'callback-example',
-        },
         {
           url: 'archive.org',
           label: html`<p class="foo">foo</p>`,
@@ -65,18 +78,56 @@ describe('IaDropdown', () => {
         html`<ia-dropdown .options=${options}></ia-dropdown>`
       );
 
+      expect(el.options.length).to.equal(1);
+      const list = el.shadowRoot?.querySelector('ul');
+
+      expect(list).to.exist;
+      expect(list?.querySelectorAll('li').length).to.equal(1);
+    });
+    it('can set `optionGroup` namespace', async () => {
+      const el = await fixture<IaDropdown>(
+        html`<ia-dropdown .optionGroup=${'foobarz'}></ia-dropdown>`
+      );
+
+      expect(el.optionGroup).to.equal('foobarz');
+
+      const srOnlyCTA = el.shadowRoot?.querySelector(
+        'button.click-main span.cta.sr-only'
+      );
+      expect(srOnlyCTA?.innerHTML).to.contain('foobarz');
+    });
+    it('On Option Selected: fires custom event `optionSelected` & option callback `selectedHandler` if available', async () => {
+      let optionCallbackReceived = false;
+      const el = await fixture<IaDropdown>(
+        html`<ia-dropdown
+          .options=${[
+            {
+              selectedHandler: () => {
+                optionCallbackReceived = true;
+              },
+              label: 'beep',
+              id: 'callback-example',
+            },
+          ] as optionInterface[]}
+        ></ia-dropdown>`
+      );
+
+      let thisOptionSelected;
+
+      const eventListener = (e: CustomEvent) => {
+        thisOptionSelected = e.detail.option as optionInterface;
+      };
+      // eslint-disable-next-line no-undef
+      el.addEventListener('optionSelected', eventListener as EventListener);
+
+      expect(thisOptionSelected).to.be.undefined;
       expect(optionCallbackReceived).to.be.false;
-      expect(el.options.length).to.equal(2);
 
       (el.shadowRoot?.querySelector('button.click-main') as any)?.click(); // TS barks but `click()` is alegit test fn
       await el.updateComplete;
       expect(el.open).to.be.true;
 
       const list = el.shadowRoot?.querySelector('ul');
-
-      expect(list).to.exist;
-      expect(list?.querySelectorAll('li').length).to.equal(2);
-
       const firstOption = list?.querySelector('li');
       const firstOptionTarget = firstOption?.querySelector('button');
       expect(firstOptionTarget).to.exist;
@@ -86,6 +137,15 @@ describe('IaDropdown', () => {
       await el.updateComplete;
       expect(optionCallbackReceived).to.be.true;
       expect(el.selectedOption).to.equal('callback-example');
+      expect(thisOptionSelected).to.exist;
+      expect(thisOptionSelected && thisOptionSelected['selectedHandler']).to
+        .exist;
+      expect(thisOptionSelected && thisOptionSelected['id']).to.equal(
+        'callback-example'
+      );
+      expect(thisOptionSelected && thisOptionSelected['label']).to.equal(
+        'beep'
+      );
     });
     it('can set `optionGroup` namespace', async () => {
       const el = await fixture<IaDropdown>(
